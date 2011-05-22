909fc7d1d4b9f508da36291a12677ed685b4b595
lixinso <lixinso@gmail.com>
2011-05-22 17:19:09 +0800

    canvas drawComplex

--- DrawImage/script.js
+++ DrawImage/script.js
@@ -31,3 +31,32 @@ function drawCycle(id)
 		context.fill();
 	}
 }
+
+function drawComplex(id)
+{
+	var canvas = document.getElementById(id);
+	if(canvas == null)
+		return false;
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#EEEEFF";
+	context.fillRect(0,0,400,300);
+	var n = 0;
+	var dx = 150;
+	var dy = 150;
+	var s = 100;
+	context.beginPath();
+	context.fillStyle = 'rgb(100,250,100)';
+	context.strokeStyle = 'rgb(0,0,100)';
+	var x = Math.sin(0);
+	var y = Math.cos(0);
+	var dig = Math.PI/15 * 11;
+	for( var i = 0 ; i < 30; i++)
+	{
+		var x = Math.sin(i*dig);
+		var y = Math.cos(i*dig);
+		context.lineTo(dx + x * s, dy + y * s);
+	}
+	context.closePath();
+	context.fill();
+	context.stroke();
+}
