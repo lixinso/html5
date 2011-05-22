185813e014ff8d52e13086b3cd8b0aae4d2ad147
lixinso <lixinso@gmail.com>
2011-05-22 17:11:22 +0800

    canvas drawCycle

--- DrawImage/script.js
+++ DrawImage/script.js
@@ -12,3 +12,22 @@ function draw(id)
 	context.fillRect(50,50,100,100);
 	context.strokeRect(50,50,100,100);
 }
+
+function drawCycle(id)
+{
+	var canvas = document.getElementById(id);
+	if(canvas == null)
+		return false;
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#EEEEFF";
+	context.fillRect(0,0,400,300);
+	var n = 0;
+	for( var i = 0 ; i < 10; i++)
+	{
+		context.beginPath();
+		context.arc(i*25,i*25,i*10,0,Math.PI*2,true);
+		context.closePath();
+		context.fillStyle = 'rgba(255,0,0,0.25)'
+		context.fill();
+	}
+}
