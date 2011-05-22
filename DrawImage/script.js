993584d73cff8ffb8dc988d7366eef815afdb286
lixinso <lixinso@gmail.com>
2011-05-22 20:45:10 +0800

    canvas drawImage

--- DrawImage/script.js
+++ DrawImage/script.js
@@ -229,3 +229,27 @@ function drawShadow(id)
 		context.fill();
 	}
 }
+
+function drawImage(id)
+{
+	var canvas = document.getElementById(id);
+	if(canvas == null)
+		return false;
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#EEEEFF";
+	context.fillRect(0,0,400,300);
+	image = new Image();
+	image.src = "pic1.jpg";
+	image.onload = function()
+	{
+		drawImg(context,image);
+	}
+}
+
+function drawImg(context,image)
+{
+	for(var i = 0 ; i < 7; i++)
+	{
+		context.drawImage(image,0+i*50,0+i*25,100,100);
+	}
+}
