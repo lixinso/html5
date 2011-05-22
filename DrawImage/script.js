369d382f05184d8ccabb84fb9055ee3f99d5627c
lixinso <lixinso@gmail.com>
2011-05-22 17:36:32 +0800

    canvas drawRotate

--- DrawImage/script.js
+++ DrawImage/script.js
@@ -85,3 +85,22 @@ function drawColorChange(id)
 		context.fill();
 	}
 }
+
+function drawRotate(id)
+{
+	var canvas = document.getElementById(id)
+	if(canvas == null)
+		return false;
+	var context = canvas.getContext('2d');
+	context.fillStyle = "#EEEEFF";
+	context.fillRect(0,0,400,300);
+	context.translate(200,50);
+	context.fillStyle='rgba(255,0,0,0.25)';
+	for(var i = 0; i < 50; i++)
+	{
+		context.translate(25,25);
+		context.scale(0.95,0.95);
+		context.rotate(Math.PI / 10);
+		context.fillRect(0,0,100,50);
+	}
+}
