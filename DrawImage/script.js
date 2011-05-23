241758f3fcb8e72177196e2fc6948bb9bbc8fcd1
lixinso <lixinso@gmail.com>
2011-05-23 21:02:54 +0800

    drawSimpleAnimation doesn't work

--- DrawImage/script.js
+++ DrawImage/script.js
@@ -362,3 +362,27 @@ function drawRevertImage(id)
 		context.putImageData(imagedata,0,0);
  	};
 }
+
+var context;
+var width,height;
+var i;
+/*doesn't work*/
+fucntion drawSimpleAnimation(id)
+{
+	var canvas = document.getElementById(id);
+	if( canvas == null)
+		return false;
+ 	context = canvas.getContext('2d');
+	width = canvas.width;
+	height = canvas.height;
+	i = 0;
+	setInterval(rotate,100);
+}
+
+function rotate()
+{
+	context.clearRect(0,0,width,height);
+	context.fillStyle = "red";
+	context.fillRect(i ,0,20,20);
+	i=i+20;
+}
